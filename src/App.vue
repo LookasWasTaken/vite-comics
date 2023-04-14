18b74c41d9fe430b1978589769bea7398d59eb7c
LookasWasTaken~ <piacen.luca@gmail.com>
2023-04-14 17:20:07 +0200

    ADD load more button

--- src/App.vue
+++ src/App.vue
@@ -26,8 +26,10 @@ export default {
       <img class="w-100" src="./assets/img/jumbotron.jpg" alt="Jumbotron">
     </div>
     <section id="main_section" class="bg-dark">
-      <div class="container py-5">
+      <div class="container py-3 text-center">
+        <h4 class="current_series">CURRENT SERIES</h4>
         <CardContainer></CardContainer>
+        <button class="load">LOAD MORE</button>
       </div>
     </section>
     <ExternalSites></ExternalSites>
@@ -37,8 +39,27 @@ export default {
 </template>
 
 <style lang="scss" scoped>
+@use "./assets/scss/partials/variables" as *;
 .jumbo{
   height: 430px;
   overflow: hidden;
+}
+
+.current_series{
+  color: $light;
+  background-color: $accent;
+  width: max-content;
+  padding: 0.5rem 1rem;
+  transform: translate(0, -80%);
+}
+
+.load{
+  font-weight: 500;
+  color: $light;
+  background-color: $accent;
+  width: max-content;
+  padding: 0.5rem 5rem;
+  border: none;
+  margin: 2rem 0.5rem;
 }
 </style>
